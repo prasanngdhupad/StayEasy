f30d3ae2aae28f4737dff9243861b7733acb1115
Prasann Gurubasappa Dhupad <158023384+prasanngdhupad@users.noreply.github.com>
2025-12-17 12:54:01 +0530

    Update cookie settings in jwtTokens.js
    Added 'path' option to cookie settings for JWT.

--- backend/utils/jwtTokens.js
+++ backend/utils/jwtTokens.js
@@ -5,13 +5,13 @@ export const sendToken = (user, statusCode, res) => {
     .status(statusCode)
     .cookie("token", token, {
       httpOnly: true,
-      secure: true,          // HTTPS only (required for SameSite=none)
-      sameSite: "none",      // 🔥 LOWERCASE (CRITICAL)
+      secure: true,
+      sameSite: "none",
+      path: "/", // 🔥 important
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     })
     .json({
       success: true,
       user,
-      token,
     });
 };
